da3b962ab9eeecf334c4c67c6510c898fce1de55
jhonD853 <131468879+jhonD853@users.noreply.github.com>
2023-08-22 19:16:37 -0500

    Add files via upload

---
+++ clase15_1/clase15_1/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] matriz = new int[5, 5];
+int agregar1 = 5;
+int agregar2 = 5;
+int suma = 0;
+int total = 0;
+int suma2   = 0;
+int total2 = 0;
+int a = 0;
+
+for (int fila = 0; fila < agregar1; fila++)
+{
+    for (int columna = 0; columna < agregar2; columna++)
+    {
+        Console.WriteLine($"Ingrese el valor para la fila {fila +1}, columna {columna +1} ");
+        matriz[fila, columna] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("elementos dentro de tu matriz:");
+
+for (int fila = 0; fila < agregar1; fila++)
+{
+    for (int columna = 0; columna < agregar2; columna++)
+    {
+        Console.Write(matriz[fila, columna] + "\t");
+    }
+
+
+    Console.WriteLine();
+}
+
+for (int i = 0; i < 5; i++)
+{
+
+    suma = matriz[i, i];
+    total = suma + total;
+    suma2 += matriz[i, 5 -1 -i];
+
+}
+Console.WriteLine($"la suma en diagonal de izquierda a derecha es: {total}");
+
+
+Console.WriteLine($"la suma en diagonal de derecha a izquierda es: {suma2}");
+
+a=suma2+total;
+Console.WriteLine($"la suma de las dos diagonales es: {a}");
+
+
+
---
+++ clase_jueves_17/clase_jueves_17/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("1 numero a romano");
+            Console.WriteLine("2 romano a numero");
+            Console.WriteLine("3 salir");
+            Console.WriteLine("escoge puto");
+            int option = int.Parse(Console.ReadLine());
+            
+            if (option == 1) {
+                Console.WriteLine("numero");
+                int numero = int.Parse(Console.ReadLine());
+                string romano = NumeroRomano(numero);
+                Console.WriteLine($"el numero es :{romano}");
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("ingrese la letra");
+                string romano= Console.ReadLine();
+                int numero = romanoNumero(romano);
+                Console.WriteLine($"el numero en romano es {romano}, en numero es {numero}");
+            }
+            else if (option == 3)
+            {
+                break;
+            }
+        }
+    }
+    static string NumeroRomano(int numero)
+    {
+        string[] romanos = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int[] Valores = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        string Resultado = "";
+        for (int i=0; i<romanos.Length; i++)
+        {
+            while(numero >= Valores[i])
+            {
+                Resultado += romanos[i];
+                numero -= Valores[i];
+            }
+        }
+        return Resultado;
+    }
+    static int romanoNumero(string romano)
+    {
+        Dictionary<char, int> valores = new Dictionary<char, int>
+        {
+            {'I', 1 }, {'V', 5 }, {'X', 10 }, {'L', 50 }, {'C', 100 },
+            {'D', 500 }, {'M', 1000 }
+        };
+        int numero = 0;
+        for (int i=0; i<romano.Length ; i++)
+        {
+            if (i+1<romano.Length && valores[romano[i]] < valores[romano[i + 1]])
+            {
+                numero-= valores[romano[i]];
+            }
+            else
+            {
+                numero += valores[romano[i]];
+            }
+        }
+        return numero;
+    }
+}
